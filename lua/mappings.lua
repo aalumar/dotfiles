ad003fb1d08ca1a41d60f096eb281fef3ad68a33
aalumar <alumarabdullah@gmail.com>
2025-09-27 18:09:46 +0300

    <leader>dw for floating diagnostic window

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,11 +1,7 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
-
 map("n", "<leader>tt", ":lua require('base46').toggle_transparency()<CR>", { noremap = true, silent = true, desc = "Toggle Background Transparency" })
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "<leader>dw", ":lua vim.diagnostic.open_float()", { noremap = true, silent = true, desc = "Show diagnostic floating window" })
