dedb6aee5ac599b8a3e36ad3b732464207ef1e3e
aalumar <alumarabdullah@gmail.com>
2026-03-04 23:51:17 +0300

    markdown renderer

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,19 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" , "dartls"}
+local servers = { "html", "cssls", "dartls", "ruff" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+vim.lsp.config("pyright", {
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "basic",
+        autoImportCompletions = true,
+        -- disableOrganizeImports stops pyright competing with ruff on imports
+        disableOrganizeImports = true,
+      },
+    },
+  },
+})
+
+vim.lsp.enable("pyright")
