ac6a1a50aac8459899f00466a4bbcb0023fa06b8
aalumar <alumarabdullah@gmail.com>
2026-04-15 13:14:54 +0300

    "nvimtree"

---
+++ lua/configs/nvimtree.lua
@@ -0,0 +1,17 @@
+local options = {
+  view = {
+    side = "right",
+  },
+  renderer = {
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+  filters = {
+    dotfiles = false, -- show dotfiles
+  },
+}
+
+return options
